23d07acced56580430ff3000231d7514a86bca8e
Gabriel Nascarella <ielnascarella@hotmail.com>
2021-11-04 19:03:34 -0300

    bruh5

---
+++ ppos_data.h
@@ -0,0 +1,84 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DINF UFPR
+// Editado por Gabriel Nascarella Hishida do Nascimento, GRR20190361
+
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __PPOS_DATA__
+#define __PPOS_DATA__
+
+#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
+#include "queue.h"		// biblioteca de filas genéricas
+#include <stdbool.h>
+
+#define AGING 1 // task aging for dynamic priority
+#define QUANTUM 1000 // a milisec.
+#define MAX_TICKS 20
+
+// Estrutura que define um Task Control Block (TCB)
+typedef struct task_t
+{
+  struct task_t *prev, *next ;		// ponteiros para usar em filas
+  ucontext_t context ;			// contexto armazenado da tarefa
+  bool is_user_task;
+  int status; // READY, DONE, ...
+  int id ;				// identificador da tarefa
+  int static_priority;
+  int dynamic_priority;
+  int clock_counter;
+
+  // Statistics:
+  unsigned int when_it_started;
+  unsigned int last_called;
+  unsigned int execution_time;
+  unsigned int processor_time;
+  unsigned int activations;
+} task_t ;
+
+
+// Possible Task Status values:
+#define READY 0
+#define DONE 1
+
+// estrutura que define um semáforo
+typedef struct
+{
+  // preencher quando necessário
+} semaphore_t ;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  // preencher quando necessário
+} barrier_t ;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  // preencher quando necessário
+} mqueue_t ;
+
+// Global vars
+// extern task_t* CURRENT_TASK;
+// extern task_t* MAIN_TASK;
+// extern int total_task_count;
+// extern int active_user_tasks;
+// extern task_t* DISPATCHER;
+// extern task_t* QUEUE;
+// extern bool DONE_CREATING_KERNEL_TASKS;
+// extern struct sigaction action;
+// extern struct itimerval timer;
+
+
+void dispatcherBody(void* arg);
+
+void alarm_handler(int signum);
+
+#endif
+
