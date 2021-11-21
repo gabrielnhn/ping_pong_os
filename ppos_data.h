e41cffd3ea40db03a82f24c69342956874136a1d
Gabriel Nascarella <ielnascarella@hotmail.com>
2021-11-21 20:04:53 -0300

    partially complete

--- ppos_data.h
+++ ppos_data.h
@@ -7,78 +7,85 @@
 #ifndef __PPOS_DATA__
 #define __PPOS_DATA__
 
-#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
-#include "queue.h"		// biblioteca de filas genéricas
+#include <ucontext.h> // biblioteca POSIX de trocas de contexto
+#include "queue.h"    // biblioteca de filas genéricas
 #include <stdbool.h>
 #include <iso646.h>
 #include <stdatomic.h>
-
+#include <math.h>
 
 // Estrutura que define um Task Control Block (TCB)
 typedef struct task_t
 {
-  struct task_t *prev, *next ;		// ponteiros para usar em filas
-  int id ;				// identificador da tarefa
+    struct task_t *prev, *next; // ponteiros para usar em filas
+    int id;                     // identificador da tarefa
 
-  ucontext_t context ;			// contexto armazenado da tarefa
-  
-  bool is_user_task;
-  int status; // READY, DONE, ...
-  int static_priority;
-  int dynamic_priority;
-  int clock_counter;
+    ucontext_t context; // contexto armazenado da tarefa
 
-  unsigned int wake_up_time;
+    bool is_user_task;
+    int status; // READY, DONE, ...
+    int static_priority;
+    int dynamic_priority;
+    int clock_counter;
 
-  queue_t* dependents;
-  int exit_code;
+    unsigned int wake_up_time;
 
-  // Statistics:
-  unsigned int when_it_started;
-  unsigned int last_called;
-  unsigned int execution_time;
-  unsigned int processor_time;
-  unsigned int activations;
-} task_t ;
+    queue_t *dependents;
+    int exit_code;
 
+    // Statistics:
+    unsigned int when_it_started;
+    unsigned int last_called;
+    unsigned int execution_time;
+    unsigned int processor_time;
+    unsigned int activations;
+} task_t;
 
 // Possible Task Status values:
 #define READY 0
 #define DONE 1
 #define SUSPENDED 2
 
-
 #define SEM_VALID 69
 #define SEM_INVALID 0
 // estrutura que define um semáforo
 typedef struct
 {
-  int counter;
-  task_t* queue;
-  atomic_flag lock;
-  int valid;
-  // preencher quando necessário
-} semaphore_t ;
+    int counter;
+    task_t *queue;
+    atomic_flag lock;
+    int valid;
+    // preencher quando necessário
+} semaphore_t;
 
 // estrutura que define um mutex
 typedef struct
 {
-  // preencher quando necessário
-} mutex_t ;
+    // preencher quando necessário
+} mutex_t;
 
 // estrutura que define uma barreira
 typedef struct
 {
-  // preencher quando necessário
-} barrier_t ;
+    // preencher quando necessário
+} barrier_t;
+
+typedef struct item_s
+{
+    struct item_s *prev, *next;
+    void *value;
+} item_t;
 
 // estrutura que define uma fila de mensagens
 typedef struct
 {
-  semaphore_t s;
-} mqueue_t ;
+    semaphore_t s;
+    item_t* queue;
+    int item_size;
+    bool destroyed;
+} mqueue_t;
 
-void dispatcherBody(void* arg);
+void dispatcherBody(void *arg);
 
 void alarm_handler(int signum);
 
@@ -90,5 +97,5 @@ void task_yield();
 
 // Global vars. to be used in `pio.c`
 
-extern task_t* CURRENT_TASK;
-extern task_t* READY_QUEUE;
+extern task_t *CURRENT_TASK;
+extern task_t *READY_QUEUE;
--- ppos_ipc.c
+++ ppos_ipc.c
@@ -1,105 +1,8 @@
 #include "ppos_data.h"
 #include <stdatomic.h>
 #include <stdio.h>
-
-
-int sem_create (semaphore_t *s, int value)
-{
-    atomic_flag_clear(&(s->lock));
-    s->valid = SEM_VALID;
-    s->counter = value;
-    s->queue = NULL;
-    return 0;
-}
-
-int sem_down (semaphore_t *s)
-{
-    if ((s == NULL) or (s->valid != SEM_VALID))
-        return -1;
-
-    // printf("BRUH\n");
-
-    while (atomic_flag_test_and_set(&(s->lock)) != false);
-    
-    s->counter -= 1;
-    // printf("%d\n", s->counter);
-
-    if (s->counter < 0)
-    {
-        queue_remove((queue_t**) &READY_QUEUE, (queue_t*) CURRENT_TASK);
-        CURRENT_TASK->status = SUSPENDED;
-        queue_append((queue_t**) &(s->queue), (queue_t*) CURRENT_TASK);
-        atomic_flag_clear(&(s->lock));
-        task_yield();
-    }
-    else
-        atomic_flag_clear(&(s->lock));
-
-    return 0;   
-}
-
-int sem_up(semaphore_t* s)
-{
-    if ((s == NULL) or (s->valid != SEM_VALID))
-        return -1;
-    
-    while (atomic_flag_test_and_set(&(s->lock)) != false);
-
-    s->counter++;
-
-    if (s->counter <= 0)
-    // Wake up first task
-    {
-        task_t* first = s->queue;
-        queue_remove((queue_t**)&(s->queue), (queue_t *) first);
-        first->status = READY;
-        queue_append((queue_t**)&READY_QUEUE, (queue_t *) first);
-    }
-
-    atomic_flag_clear(&(s->lock));
-
-    return 0;
-}
-
-int sem_destroy (semaphore_t *s)
-{
-    if ((s == NULL) or (s->valid != SEM_VALID))
-        return -1;
-
-    while (atomic_flag_test_and_set(&(s->lock)) != false);
-
-    // Wake up every single task
-
-    if (queue_size((queue_t*) s->queue) > 0)
-    {
-        queue_t* first = (queue_t*) s->queue;
-        queue_t* node = first;
-        int counter = 0;
-
-        do 
-        {
-            task_t* task = (task_t*) node;
-            node = node->next;
-            task->status = READY;
-
-            queue_remove((queue_t**)&s->queue, (queue_t *)task);
-            queue_append((queue_t**)&READY_QUEUE, (queue_t *)task);
-            
-            counter++;
-        } while (s->queue != NULL);
-        
-        // } while ((queue_size((queue_t*) s->queue) > 0) and (counter < initial_size));
-    }
-    // Destroy
-    atomic_flag_clear(&(s->lock));
-    s->valid = SEM_INVALID;
-    return 0;
-}
-
-#include "ppos_data.h"
-#include <stdatomic.h>
-#include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 int sem_create (semaphore_t *s, int value)
 {
@@ -196,5 +99,92 @@ int sem_destroy (semaphore_t *s)
 
 int mqueue_create (mqueue_t *queue, int max_msgs, int msg_size)
 {
+    if (queue == NULL)
+        return -1;
+
+    sem_create(&(queue->s), max_msgs);
+    queue->queue = NULL;
+    queue->item_size = msg_size;
+    queue->destroyed = false;
+    return 0;
+}
+
+int mqueue_send (mqueue_t *queue, void *msg)
+{
+    sem_down(&(queue->s));
+
+    // sanity checks
+    if (queue == NULL)
+        return -1;
+
+    if (queue->destroyed == true)
+        return -1;
+
+    item_t* item = malloc(sizeof(item_t));
     
+    // another sanity check
+    if (item == NULL)
+        return -1;
+    
+    item->next = NULL;
+    item->prev = NULL;
+
+    memcpy(&(item->value), msg, queue->item_size);
+    queue_append((queue_t**) &(queue->queue), (queue_t*) item);
+    return 0;
+}
+
+int mqueue_recv (mqueue_t *queue, void *msg)
+{
+    sem_up(&(queue->s));
+    item_t* first = queue->queue;
+
+    // sanity checks
+    if (queue == NULL)
+        return -1;
+    
+    if (queue->queue == NULL)
+        return -1;
+    
+    if (queue->destroyed == true)
+        return -1;
+
+    queue_remove((queue_t**) &(queue->queue), (queue_t*) first);
+    memcpy(msg, &(first->value), queue->item_size);
+    
+    free(first);
+
+    return 0;
+}
+
+int mqueue_destroy (mqueue_t *queue)
+{
+    if (queue->destroyed == true)
+        return -1;
+
+    queue->destroyed = true;
+    sem_destroy(&(queue->s));
+
+    // Destroy every message
+
+    if (queue_size((queue_t*) queue->queue) > 0)
+    {
+        queue_t* first = (queue_t*) queue->queue;
+        queue_t* node = first;
+        int counter = 0;
+
+        do 
+        {
+            item_t* item = (item_t*) node;
+            node = node->next;
+
+            queue_remove((queue_t**)&queue->queue, (queue_t *)item);
+            free(item);
+            
+            counter++;
+        } while (queue->queue != NULL);
+        
+
+    }
+    return 0;
 }
